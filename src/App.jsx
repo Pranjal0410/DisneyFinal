88138eee8c7f0067655384d622802b8fe9561f71
Pranjal Udhwani <udhwanipranjal@gmail.com>
2024-09-30 18:06:51 +0530

    first evaluation completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import './index.css';
 import Login from './components/Login';
 import Home from './components/Home';
 import Header from './components/Header';
+import FAQ from './components/FAQ'; // Correctly import the FAQ component
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 
 export default function App() {
   return (
@@ -15,6 +15,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/faq" element={<FAQ />} /> {/* Use FAQ component here */}
         </Routes>
       </Router>
     </div>
--- src/components/pastevents.jsx
+++ src/components/pastevents.jsx
@@ -1,23 +1,23 @@
 import styled from 'styled-components';
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const NewDisney = (props) => {
   return (
     <Container>
-      <h4>PAST EVENTS</h4>
+      <Title>PAST EVENTS</Title>
       <Content>
         <Wrap>
           <img src='WhatsApp Image 2024-08-27 at 12.50.20_2acd3c94.jpg' alt='' />
-          </Wrap>
-          <Wrap>
+        </Wrap>
+        <Wrap>
           <img src='WhatsApp Image 2024-09-01 at 23.22.26_1b728b25.jpg' alt='' />
-          </Wrap>
-          <Wrap>
+        </Wrap>
+        <Wrap>
           <img src='WhatsApp Image 2024-09-01 at 23.22.27_bd5f6b77.jpg' alt='' />
-          </Wrap>
-          <Wrap>
+        </Wrap>
+        <Wrap>
           <img src='Screenshot 2024-09-02 003343.png' alt='' />
-          </Wrap>
+        </Wrap>
       </Content>
     </Container>
   );
@@ -26,7 +26,6 @@ const NewDisney = (props) => {
 const Container = styled.div`
   padding: 0 0 26px;
   font: 400 16px/24px 'Roboto', sans-serif;
-
 `;
 
 const Content = styled.div`
@@ -39,26 +38,31 @@ const Content = styled.div`
   }
 `;
 
-export default NewDisney;
-
 const Wrap = styled.div`
   border-radius: 10px;
-  cursor;
+  cursor: pointer;
   overflow: hidden;
   border: 3px solid rgba(249, 249, 249, 0.1);
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-  transtition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
 
   img {
     width: 100%;
     height: 100%;
     object-fit: cover;
   }
+
   &:hover {
     transform: scale(1.05);
     border-color: rgba(249, 249, 249, 0.8);
   }
-  
+`;
 
+const Title = styled.h4`
+  text-align: center;
+  font-size: 1.8em; 
+  margin-bottom: 20px; 
+  color: inherit; 
+`;
 
-`
+export default NewDisney;
